ae2d1e27841f310b9eb0122932a36c4871f86090
Juan Pablo Justiniano <jpjustiniano@gmail.com>
2011-04-01 18:09:32 -0300

    Sunae cambiado a programa con subrutina incluida en vez de solo la subrutina

--- sunae.f90
+++ sunae.f90
@@ -1,18 +1,91 @@
+! Copyright (C) 2011, Juan Pablo Justiniano  <jpjustiniano@gmail.com>
+!
+Program TestSunae
+implicit none
 
- SUBROUTINE sunae(year,day,hour, lat, long,az,el,ha,dec,soldst)  ! Sun's position, Michalsky
- !Real, intent(in) :: year
- !Real, intent(in) :: day
- !Real, intent(in) :: hour
- !Real, intent(in) :: lat
- !Real, intent(in) :: long
- !Real, intent(out) :: az
- !Real, intent(out) :: el
- !Real, intent(out) :: ha
- !Real, intent(out) :: dec
- !Real, intent(out) :: soldst
+ Real :: year
+ Real :: day
+ Real :: hour
+ Real :: lat
+ Real :: lon
+ Real :: az
+ Real :: el
+ Real :: ha
+ Real :: dec
+ Real :: soldst
 
-implicit real (a-z)
-data twopi,pi,rad/6.2831853,3.1415927,.017453293/
+	Year = 2011
+	lat = -33.45
+	lon= -70.66
+	
+	write(*,*)
+40	write(*,*) 'day of year:  '
+	read (*,*) day
+50	write(*,*) 'time:       (UTC)  (99 = exit) (999 = change day)'
+	read (*,*) hour
+	If (hour == 99) goto 99
+	If (hour == 999) goto 40
+	
+	call sunae(year,day,hour,lat,lon,az,el,ha,dec,soldst)
+	write (*,*)
+	If (el<0.) then
+		print *,' Night time!! Remember, UTC decimal hours.!!'
+		go to 50
+	end if	
+	write (*,100) az,el,ha,dec,soldst
+	write (*,*)
+	goto 50
+	
+100 Format (' Azimuth:    ',F6.1,/' Elevation:  ', F4.1,/' Hour angle: ',F5.1,/ &
+			& ' Declination: ', F4.1,/' Sun dist.:   ',F4.1)
+
+99 stop
+
+
+ contains
+!-------------------------------------------------------------------------------+
+!  This subroutine calculates the local azimuth and elevation of the sun at		|
+!  a specific location and time using an approximation to equations used   		|
+!  to generate tables in The Astronomical Almanac.  				  	   		|
+!  Refraction correction is added so sun position is apparent one. 		   		|
+!                                                                      			|
+!The Astronomical Almanac, U.S. Gov't Printing Office, Washington,D.C. (1985).	|
+!                                                                      			|
+!    input parameters                                                  			|
+!      year=year, e.g., 1986                                           			|
+!      day=day of year, e.g., feb 1=32                                 			|
+!      hour=hours plus fraction in UT, e.g., 8:30 am eastern daylight  			|
+!        time is equal to 8.5 + 5(5 hours west of Greenwich) -1(for    			|
+!        daylight savings time correction)                             			|
+!      lat=latitude in degrees (north is positive)                     			|
+!      long=longitude in degrees (east is positive)                    			|
+!                                                                      			|
+!    output parameters                                                 			|
+!	az= sun azimuth angle (measured east from north, 0 to 360 degs)				|
+!	el= sun elevation angle (degs)                                 				|
+!	ha= solar hour angle														|
+!	dec = declination															|
+!	soldst = solar distance                                        				|				
+!-------------------------------------------------------------------------------+
+ Subroutine sunae(year,day,hour, lat, long,az,el,ha,dec,soldst)  ! Sun's position, Michalsky
+ implicit none
+ Real, intent(in) :: year
+ Real, intent(in) :: day
+ Real, intent(in) :: hour
+ Real, intent(in) :: lat
+ Real, intent(in) :: long
+ Real, intent(out) :: az
+ Real, intent(out) :: el
+ Real, intent(out) :: ha
+ Real, intent(out) :: dec
+ Real, intent(out) :: soldst
+ 
+ real :: twopi, pi, rad
+ real :: delta, leap, jd, time
+ real :: mnlong, mnanom, eclong, oblqec, num, den, ra
+ real :: gmst, lmst, latrad, elc, refrac, soldia
+
+ data twopi,pi,rad/6.2831853,3.1415927,.017453293/
   
 !   get the current julian date (actually add 2,400,000 for jd)
       delta=year-1949.
@@ -81,15 +154,15 @@ data twopi,pi,rad/6.2831853,3.1415927,.017453293/
 
 !   calculate azimuth and elevation
       el=asin(sin(dec)*sin(latrad)+cos(dec)*cos(latrad)*cos(ha))
-!      az=asin(-cos(dec)*sin(ha)/cos(el))
+      az=asin(-cos(dec)*sin(ha)/cos(el))
 
 !!   this puts azimuth between 0 and 2*pi radians
-      if(sin(dec)-sin(el)*sin(latrad).ge.0.) then
+      if (sin(dec)-sin(el)*sin(latrad).ge.0.) then
 		if(sin(az).lt.0.) az=az+twopi
       else
       az=pi-az
       endif
-   if az=90 degs, elcritical=asin(sin(dec)/sin(latrad))
+!   if az=90 degs, elcritical=asin(sin(dec)/sin(latrad))
     elc=asin(sin(dec)/sin(latrad))
     if(el.ge.elc)az=pi-az
     if(el.le.elc.and.ha.gt.0.)az=twopi+az
@@ -101,8 +174,7 @@ data twopi,pi,rad/6.2831853,3.1415927,.017453293/
       if(el.ge.19.225) then 
          refrac=.00452*3.51823/tan(el*rad)
       else if (el.gt.-.766.and.el.lt.19.225) then
-         refrac=3.51823*(.1594+.0196*el+.00002*el**2)/ &
-     &   (1.+.505*el+.0845*el**2)
+         refrac=3.51823*(.1594+.0196*el+.00002*el**2)/(1.+.505*el+.0845*el**2)
       else if (el.le.-.766) then
          refrac=0.0
       end if
@@ -115,11 +187,12 @@ data twopi,pi,rad/6.2831853,3.1415927,.017453293/
       soldia=.5332/soldst
 
 !   convert az and lat to degs before returning
-      	az=az/rad
-      	lat=lat/rad
+    az=az/rad
 	ha=ha/rad
 	dec=dec/rad
 
 !   mnlong in degs, gmst in hours, jd in days if 2.4e6 added;
 !   mnanom,eclong,oblqec,ra,and lmst in radians
 End subroutine
+
+end program TestSunae
